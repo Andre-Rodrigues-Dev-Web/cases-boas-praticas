80f671289734d91e243a7de8241b20091edaeaf8
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-08-24 19:17:18 -0300

    feat: código if javascript

---
+++ boas-praticas-js/casos-ifs.js
@@ -0,0 +1,69 @@
+function verificarDiaDoMes(dia) {
+    if (dia === 1) {
+        console.log("Hoje é dia 1.");
+    } else if (dia === 2) {
+        console.log("Hoje é dia 2.");
+    } else if (dia === 3) {
+        console.log("Hoje é dia 3.");
+    } else if (dia === 4) {
+        console.log("Hoje é dia 4.");
+    } else if (dia === 5) {
+        console.log("Hoje é dia 5.");
+    } else if (dia === 6) {
+        console.log("Hoje é dia 6.");
+    } else if (dia === 7) {
+        console.log("Hoje é dia 7.");
+    } else if (dia === 8) {
+        console.log("Hoje é dia 8.");
+    } else if (dia === 9) {
+        console.log("Hoje é dia 9.");
+    } else if (dia === 10) {
+        console.log("Hoje é dia 10.");
+    } else if (dia === 11) {
+        console.log("Hoje é dia 11.");
+    } else if (dia === 12) {
+        console.log("Hoje é dia 12.");
+    } else if (dia === 13) {
+        console.log("Hoje é dia 13.");
+    } else if (dia === 14) {
+        console.log("Hoje é dia 14.");
+    } else if (dia === 15) {
+        console.log("Hoje é dia 15.");
+    } else if (dia === 16) {
+        console.log("Hoje é dia 16.");
+    } else if (dia === 17) {
+        console.log("Hoje é dia 17.");
+    } else if (dia === 18) {
+        console.log("Hoje é dia 18.");
+    } else if (dia === 19) {
+        console.log("Hoje é dia 19.");
+    } else if (dia === 20) {
+        console.log("Hoje é dia 20.");
+    } else if (dia === 21) {
+        console.log("Hoje é dia 21.");
+    } else if (dia === 22) {
+        console.log("Hoje é dia 22.");
+    } else if (dia === 23) {
+        console.log("Hoje é dia 23.");
+    } else if (dia === 24) {
+        console.log("Hoje é dia 24.");
+    } else if (dia === 25) {
+        console.log("Hoje é dia 25.");
+    } else if (dia === 26) {
+        console.log("Hoje é dia 26.");
+    } else if (dia === 27) {
+        console.log("Hoje é dia 27.");
+    } else if (dia === 28) {
+        console.log("Hoje é dia 28.");
+    } else if (dia === 29) {
+        console.log("Hoje é dia 29.");
+    } else if (dia === 30) {
+        console.log("Hoje é dia 30.");
+    } else if (dia === 31) {
+        console.log("Hoje é dia 31.");
+    } else {
+        console.log("Dia inválido.");
+    }
+}
+
+verificarDiaDoMes(15);
